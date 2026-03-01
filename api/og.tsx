97cf6f0526a90f151830e3470f673592b10e768d
akasai <akasai>
2026-03-02 00:56:13 +0900

    fix: OG 이미지 렌더링 환경 수정
    - `runtime` 설정을 `edge`에서 `nodejs`로 변경
      - `@vercel/og`의 렌더링 환경에 맞게 업데이트

--- api/og.tsx
+++ api/og.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from '@vercel/og'
 
 
-export const config = { runtime: 'edge' }
+export const config = { runtime: 'nodejs' }
 
 async function loadGoogleFont(
     font: string,
